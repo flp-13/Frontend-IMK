30b72d22f51432b74773b2c1f29f517ca3f086e4
Felipe <felipelie97@gmail.com>
2020-12-12 10:56:05 +0700

    front end v1

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("assets"));
+
+app.get("/", (req,res)=>{
+    res.render("index", {curPage: "index"});
+});
+
+app.get("/berita", (req,res)=>{
+    res.render("berita", {curPage: "berita"});
+});
+
+app.get("/berita/:id", (req,res)=>{
+    res.render("detail-berita", {curPage: "berita"});
+});
+
+app.get("/contact", (req,res)=>{
+    res.render("contact", {curPage: "contact"});
+});
+
+app.get("/login", (req,res)=>{
+    res.render("login");
+});
+
+app.listen(3000, ()=>{
+    console.log("app is running");
+});
